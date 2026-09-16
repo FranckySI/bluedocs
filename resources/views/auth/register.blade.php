2e99333a1a7e93597b2f0d2bbb0f2b4f5520cb8f
FranckySI <francky.andriatiana@staff.blueline.mg>
2026-09-16 11:28:13 +0300

    modif creation user : editeur only

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -17,7 +17,6 @@
                 class="block mt-1 w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm"
                 required>
                 <option value="user">Editeur</option>
-                <option value="admin">Administrateur</option>
             </select>
             <x-input-error :messages="$errors->get('role')" class="mt-2" />
         </div>
